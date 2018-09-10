63a964725c38a98d4349bb17ef8f507c357567b1
SomeRandomPizzaGuy <edvin.gjervaldsaeter@falun.ntig.se>
2018-09-10 08:50:22 +0200

    Apartment Update

--- Apartment_Prog_2/Apartment_Prog_2/Program.cs
+++ Apartment_Prog_2/Apartment_Prog_2/Program.cs
@@ -15,6 +15,10 @@ namespace Apartment_Prog_2
         static int apartmentOneRoomCount = 0;
         static int apartmentTwoRoomCount = 0;
         static int apartmentThreeRoomCount = 0;
+        static int apartmentFourRoomCount = 0;
+        static int apartmentFiveRoomCount = 0;
+        static int apartmentSixRoomCount = 0;
+        static int apartmentSevenRoomCount = 0;
 
         static void Main(string[] args)
         {
@@ -43,6 +47,11 @@ namespace Apartment_Prog_2
                         Console.WriteLine();
                         Console.WriteLine("Aparment 1 have " + apartmentOneRoomCount + " rooms");
                         Console.WriteLine("Aparment 2 have " + apartmentTwoRoomCount + " rooms");
+                        Console.WriteLine("Aparment 3 have " + apartmentThreeRoomCount + " rooms");
+                        Console.WriteLine("Aparment 4 have " + apartmentFourRoomCount + " rooms");
+                        Console.WriteLine("Aparment 5 have " + apartmentFiveRoomCount + " rooms");
+                        Console.WriteLine("Aparment 6 have " + apartmentSixRoomCount + " rooms");
+                        Console.WriteLine("Aparment 7 have " + apartmentSevenRoomCount + " rooms");
                         Console.WriteLine("How many rooms you have left: " + roomAmaunt);
                         Console.ReadLine();
                     }
@@ -65,9 +74,6 @@ namespace Apartment_Prog_2
                 }
             }
             while (userMainMenuChoise != 0);
-
-            Console.WriteLine("Shutting down the progam");
-            Console.ReadLine();
         }
 
         static void MainApartmentMenu(int roomAmount = 20)
@@ -97,28 +103,23 @@ namespace Apartment_Prog_2
                     }
                     else if (userMainMenuChoise == 3)
                     {
-                        Console.WriteLine("Apartment 3");
-                        Console.ReadLine();
+                        ApartmentThree();
                     }
                     else if (userMainMenuChoise == 4)
                     {
-                        Console.WriteLine("Apartment 4");
-                        Console.ReadLine();
+                        ApartmentFour();
                     }
                     else if (userMainMenuChoise == 5)
                     {
-                        Console.WriteLine("Apartment 5");
-                        Console.ReadLine();
+                        ApartmentFive();
                     }
                     else if (userMainMenuChoise == 6)
                     {
-                        Console.WriteLine("Apartment 6");
-                        Console.ReadLine();
+                        ApartmentSix();
                     }
                     else if (userMainMenuChoise == 7)
                     {
-                        Console.WriteLine("Apartment 7");
-                        Console.ReadLine();
+                        ApartmentSeven();
                     }
                     else if (userMainMenuChoise == 0)
                     {
@@ -155,6 +156,7 @@ namespace Apartment_Prog_2
                 Console.WriteLine("Choose -1 to subtract a room");
                 Console.WriteLine("Choose 0 to move back");
                 Console.WriteLine("This apartment currently has " + apartmentOneRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
                 Console.Write("Write your choise: ");
 
                 userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
@@ -227,6 +229,7 @@ namespace Apartment_Prog_2
                 Console.WriteLine("Choose -1 to subtract a room");
                 Console.WriteLine("Choose 0 to move back");
                 Console.WriteLine("This apartment currently has " + apartmentTwoRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
                 Console.Write("Write your choise: ");
 
                 userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
@@ -283,6 +286,371 @@ namespace Apartment_Prog_2
             while (userMainMenuChoise != 0);
 
             return apartmentTwoRoomCount;
+        }
+
+        static int ApartmentThree()
+        {
+
+            int userMainMenuChoise = -2;
+            bool userMainMenuChoiseCheck;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("This is Appartment 3");
+                Console.WriteLine("Choose 1 to ad a room");
+                Console.WriteLine("Choose -1 to subtract a room");
+                Console.WriteLine("Choose 0 to move back");
+                Console.WriteLine("This apartment currently has " + apartmentThreeRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
+                Console.Write("Write your choise: ");
+
+                userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
+
+                if (userMainMenuChoiseCheck)
+                {
+                    if (userMainMenuChoise == 1)
+                    {
+                        if (roomAmaunt > 0)
+                        {
+                            apartmentThreeRoomCount++;
+
+                            roomAmaunt--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough spare rooms, subtract some rooms from other " +
+                                "apartments to add here");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == -1)
+                    {
+                        if (apartmentThreeRoomCount > 0)
+                        {
+                            apartmentThreeRoomCount--;
+
+                            roomAmaunt++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough rooms, an apartment can't have negitive rooms");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Was unable to find that choise");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    userMainMenuChoise = -1;
+
+                    Console.WriteLine("Nubers only, try again");
+                    Console.ReadLine();
+                }
+            }
+            while (userMainMenuChoise != 0);
+
+            return apartmentThreeRoomCount;
+        }
+
+        static int ApartmentFour()
+        {
+
+            int userMainMenuChoise = -2;
+            bool userMainMenuChoiseCheck;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("This is Appartment 4");
+                Console.WriteLine("Choose 1 to ad a room");
+                Console.WriteLine("Choose -1 to subtract a room");
+                Console.WriteLine("Choose 0 to move back");
+                Console.WriteLine("This apartment currently has " + apartmentFourRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
+                Console.Write("Write your choise: ");
+
+                userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
+
+                if (userMainMenuChoiseCheck)
+                {
+                    if (userMainMenuChoise == 1)
+                    {
+                        if (roomAmaunt > 0)
+                        {
+                            apartmentFourRoomCount++;
+
+                            roomAmaunt--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough spare rooms, subtract some rooms from other " +
+                                "apartments to add here");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == -1)
+                    {
+                        if (apartmentFourRoomCount > 0)
+                        {
+                            apartmentFourRoomCount--;
+
+                            roomAmaunt++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough rooms, an apartment can't have negitive rooms");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Was unable to find that choise");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    userMainMenuChoise = -1;
+
+                    Console.WriteLine("Nubers only, try again");
+                    Console.ReadLine();
+                }
+            }
+            while (userMainMenuChoise != 0);
+
+            return apartmentFourRoomCount;
+        }
+
+        static int ApartmentFive()
+        {
+
+            int userMainMenuChoise = -2;
+            bool userMainMenuChoiseCheck;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("This is Appartment 5");
+                Console.WriteLine("Choose 1 to ad a room");
+                Console.WriteLine("Choose -1 to subtract a room");
+                Console.WriteLine("Choose 0 to move back");
+                Console.WriteLine("This apartment currently has " + apartmentFiveRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
+                Console.Write("Write your choise: ");
+
+                userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
+
+                if (userMainMenuChoiseCheck)
+                {
+                    if (userMainMenuChoise == 1)
+                    {
+                        if (roomAmaunt > 0)
+                        {
+                            apartmentFiveRoomCount++;
+
+                            roomAmaunt--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough spare rooms, subtract some rooms from other " +
+                                "apartments to add here");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == -1)
+                    {
+                        if (apartmentFiveRoomCount > 0)
+                        {
+                            apartmentFiveRoomCount--;
+
+                            roomAmaunt++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough rooms, an apartment can't have negitive rooms");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Was unable to find that choise");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    userMainMenuChoise = -1;
+
+                    Console.WriteLine("Nubers only, try again");
+                    Console.ReadLine();
+                }
+            }
+            while (userMainMenuChoise != 0);
+
+            return apartmentFiveRoomCount;
+        }
+
+        static int ApartmentSix()
+        {
+
+            int userMainMenuChoise = -2;
+            bool userMainMenuChoiseCheck;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("This is Appartment 6");
+                Console.WriteLine("Choose 1 to ad a room");
+                Console.WriteLine("Choose -1 to subtract a room");
+                Console.WriteLine("Choose 0 to move back");
+                Console.WriteLine("This apartment currently has " + apartmentSixRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
+                Console.Write("Write your choise: ");
+
+                userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
+
+                if (userMainMenuChoiseCheck)
+                {
+                    if (userMainMenuChoise == 1)
+                    {
+                        if (roomAmaunt > 0)
+                        {
+                            apartmentSixRoomCount++;
+
+                            roomAmaunt--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough spare rooms, subtract some rooms from other " +
+                                "apartments to add here");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == -1)
+                    {
+                        if (apartmentSixRoomCount > 0)
+                        {
+                            apartmentSixRoomCount--;
+
+                            roomAmaunt++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough rooms, an apartment can't have negitive rooms");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Was unable to find that choise");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    userMainMenuChoise = -1;
+
+                    Console.WriteLine("Nubers only, try again");
+                    Console.ReadLine();
+                }
+            }
+            while (userMainMenuChoise != 0);
+
+            return apartmentSixRoomCount;
+        }
+
+        static int ApartmentSeven()
+        {
+
+            int userMainMenuChoise = -2;
+            bool userMainMenuChoiseCheck;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("This is Appartment 7");
+                Console.WriteLine("Choose 1 to ad a room");
+                Console.WriteLine("Choose -1 to subtract a room");
+                Console.WriteLine("Choose 0 to move back");
+                Console.WriteLine("This apartment currently has " + apartmentSevenRoomCount + " rooms");
+                Console.WriteLine("You have " + roomAmaunt + " rooms left");
+                Console.Write("Write your choise: ");
+
+                userMainMenuChoiseCheck = int.TryParse(Console.ReadLine(), out userMainMenuChoise);
+
+                if (userMainMenuChoiseCheck)
+                {
+                    if (userMainMenuChoise == 1)
+                    {
+                        if (roomAmaunt > 0)
+                        {
+                            apartmentSevenRoomCount++;
+
+                            roomAmaunt--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough spare rooms, subtract some rooms from other " +
+                                "apartments to add here");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == -1)
+                    {
+                        if (apartmentSevenRoomCount > 0)
+                        {
+                            apartmentSevenRoomCount--;
+
+                            roomAmaunt++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough rooms, an apartment can't have negitive rooms");
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (userMainMenuChoise == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Was unable to find that choise");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    userMainMenuChoise = -1;
+
+                    Console.WriteLine("Nubers only, try again");
+                    Console.ReadLine();
+                }
+            }
+            while (userMainMenuChoise != 0);
+
+            return apartmentSevenRoomCount;
         }
     }
 }
